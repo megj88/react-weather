5f4b0faa33a8b4cd07dc12b614032c51c8c8abb4
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-02 10:23:16 +0100

    Add footer with github link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
        <h1>Weather Search Engine</h1>
        <SearchEngine />
       </header>
+      <footer>
+        Open-source code by <a href="https://github.com/megj88/react-weather" target="_blank">Megan</a>
+      </footer>
     </div>
   );
 }
