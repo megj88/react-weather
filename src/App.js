6377b019510741636f883f58aa31aa0e07fdd3d2
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-02 18:30:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="AppHeader">
        <h1>Weather Search Engine</h1>
        <SearchEngine />
-       <p>Open-source code by <a href="https://github.com/megj88/react-weather" target="_blank">Megan</a></p>
       </header>
 
     </div>
